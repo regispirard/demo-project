990a65449e878e4b902ebffd4dcbc678d6992ae8
regispirard <regis.pirard@tincid.com>
2023-11-15 22:27:37 +0100

    Bump version: 0.0.5 → 0.0.6

--- odoo/addons/demo_all/__manifest__.py
+++ odoo/addons/demo_all/__manifest__.py
@@ -7,7 +7,7 @@
     Demo Odoo Project - All addons and dependencies""",
     "website": "",
     "category": "demo",
-    "version": "16.0.0.0.5",
+    "version": "16.0.0.0.6",
     "license": "AGPL-3",
     "depends": [
         # Odoo addons - no odoo enterprise addons dependencies !!!
